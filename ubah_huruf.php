c4501acbab2280f9e5b9889fc4162dd02e127c2c
Dimas Lazuardy Rizal <dymaz39@gmail.com>
2020-07-30 10:36:12 +0700

    php-3

---
+++ ubah_huruf.php
@@ -0,0 +1,20 @@
+<?php
+function ubah_huruf($string){
+//kode di sini
+    $alfabet="abcdefghijklmnopqrstuvwxyz";
+    $output="";
+    for($i=0; $i<strlen($string); $i++){
+        $position = strrpos($alfabet,$string[$i]);
+        $output.= substr($alfabet,$position+1,1);
+    }
+    return $output;
+}
+
+// TEST CASES
+echo ubah_huruf('wow'); // xpx
+echo "<br>".ubah_huruf('developer'); // efwfmpqfs
+echo "<br>".ubah_huruf('laravel'); // mbsbwfm
+echo "<br>".ubah_huruf('keren'); // lfsfo
+echo "<br>".ubah_huruf('semangat'); // tfnbohbu
+
+?>
