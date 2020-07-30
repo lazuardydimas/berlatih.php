6902e83bd31e97430121c4f809fb00c4746ee14a
Dimas Lazuardy Rizal <dymaz39@gmail.com>
2020-07-30 10:14:19 +0700

    php-3

--- pasangan-terbesar.php
+++ pasangan-terbesar.php
@@ -1,20 +1,21 @@
 <?php
 $pair= [];
 function pasangan_terbesar($angka){
-$ang =(string)$angka;
-explode('',$ang,3);
-for($i=0;$i<count($ang)-1;$i++){
-    if($i===0) $pair= $ang[$i]+ $ang[$i+1];
-    if($pair< (int)$ang[$i]+$ang[$i+1]) $pair= (int)$ang[i]+ $ang[$i+1];
-}
- return (int)$pair;
+    $angka_terbesar = substr($angka,0,2);
+    for($i=0;$i<= strlen($angka)-2; $i++){
+    $pasangan_angka= substr($angka,$i,2);
+    if($angka_terbesar< $pasangan_angka){
+        $angka_terbesar = $pasangan_angka;
+        }
+    }
+    return $angka_terbesar;
 }
 
 // TEST CASES
 echo pasangan_terbesar(641573); // 73
-echo pasangan_terbesar(12783456); // 83
-echo pasangan_terbesar(910233); // 91
-echo pasangan_terbesar(71856421); // 85
-echo pasangan_terbesar(79918293); // 99
+echo "<br>". pasangan_terbesar(12783456); // 83
+echo "<br>". pasangan_terbesar(910233); // 91
+echo "<br>". pasangan_terbesar(71856421); // 85
+echo "<br>". pasangan_terbesar(79918293); // 99
 
 ?>
